bf7935c16ecdd96751041ff6f18cfb3d743adf15
Vitor Munaretto <vitormunarettoads@hotmail.com>
2023-07-19 18:24:18 -0300

    Editado Home

--- app/Controllers/Pages/HomeController.php
+++ app/Controllers/Pages/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controllers\Pages;
 
+use App\Controllers\ComponentsController;
+use App\Models\Entitys\Atendimento;
+use App\Models\Repository\AtendimentoRepository;
 use App\Utils\View;
 
 class HomeController extends PageController{
@@ -11,8 +14,26 @@ class HomeController extends PageController{
      */
     public static function getHome() : string
     {
+        $atendimentos = (new AtendimentoRepository())->findBy([], null, 5);
+        $textAtendimentos = '';
+
+        /** @var Atendimento */
+        foreach($atendimentos as $atendimento){
+            $textAtendimentos .= View::render('home/item', [
+                'clinica' => $atendimento->getClinica()->getNome(),
+                'profissional' => $atendimento->getProfissionalClinica()->getProfissional()->getNome(),
+                'animal' => $atendimento->getAnimal()->getNome(),
+                'responsavelAnimal' => $atendimento->getAnimal()->getResponsavelPadrao() ? $atendimento->getAnimal()->getResponsavelPadrao()->getResponsavel()->getNome() : '',
+                'linkAtendimento' => ComponentsController::createButton('atendimento/editar/' . $atendimento->getId(), 'primary', 'Acessar Atendimento')
+            ]);
+        }
+
+        $table = View::render('home/table', [
+            'atendimentos' => $textAtendimentos
+        ]);
+
         $content = View::render('home/home', [
-            'name' => 'teste'
+            'table' => $table
         ]);
 
         return parent::getPage('Home', $content);
